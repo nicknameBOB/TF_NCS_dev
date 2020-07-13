2df5cc30853219445262a8da585775597f4d076d
Jonathan <jonath@live.no>
2020-07-13 15:25:41 +0200

    init

--- tensorflow_hello_world/build/spm/shared_vars.cmake
+++
@@ -1,7 +0,0 @@
-set(spm_KERNEL_HEX_NAME zephyr.hex)
-set(spm_ZEPHYR_BINARY_DIR /home/jolu/ncs/TF_NCS_dev/tensorflow_hello_world/build/spm/zephyr)
-set(spm_KERNEL_ELF_NAME zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS /home/jolu/ncs/TF_NCS_dev/tensorflow_hello_world/build/spm/zephyr/zephyr.hex)
-list(APPEND spm_BUILD_BYPRODUCTS /home/jolu/ncs/TF_NCS_dev/tensorflow_hello_world/build/spm/zephyr/zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS
-    /home/jolu/ncs/TF_NCS_dev/tensorflow_hello_world/build/spm/libspmsecureentries.a)
