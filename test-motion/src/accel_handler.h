05c15b5bc7335e9b7d971a7175c93a1d4cc6487b
Jonathan <jonath@live.no>
2020-08-11 16:25:52 +0200

    WIP-remote

---
+++ test-motion/src/accel_handler.h
@@ -0,0 +1,10 @@
+
+#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_OUTPUT_HANDLER_H_
+#define TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_ACCEL
+
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+
+void HandleOutput();
+
+#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_OUTPUT_HANDLER_H_
